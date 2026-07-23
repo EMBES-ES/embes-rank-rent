56e42ba229ccaf8aee851c1b866c2b1036833c19
Mohamed Al Howaidi Nasralla <54993881+muya03@users.noreply.github.com>
2026-07-23 19:59:08 +0200

    Delete src directory

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-// Helper de shadcn/ui: combina clases de Tailwind resolviendo conflictos.
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
